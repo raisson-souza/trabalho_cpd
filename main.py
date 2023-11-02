49e63ad5561d31caea801b0f147df31b3a0c6e36
Raisson Souza <raissonrai@gmail.com>
2023-11-02 18:03:42 -0300

    Arvore binária de busca de preço criada

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 # fazer por igualdade e depois ir por intervalo
 # nao vale build in
 from datetime import datetime
-from generate_random_games import generate_random_games
+import generate_random_games
+
+def generate_games():
+    initial = datetime.now()
+    games = generate_random_games.generate_random_games(1000)
+    final = datetime.now()
+    print(final - initial)
+    return games
 
 class Node:
-    Game = None
+    Game = generate_random_games.Game
     L = None # ESQUERDO
     R = None # DIREITO
 
-    def __init__(self, game):
+    def __init__(self, game : generate_random_games.Game):
         self.Game = game
         self.L = None
         self.R = None
 
-initial = datetime.now()
-games = generate_random_games(100000)
-final = datetime.now()
-print(final - initial)
-# input()
+def print_bst(root : Node):
+    if root is None:
+        return
+    print_bst(root.L)
+    print(f"Jogo: { root.Game.Title } | Preço: { root.Game.Price }")
+    print_bst(root.R)
+
+def insert_price_bst(root : Node, game : generate_random_games.Game):
+    if root == None:
+        return Node(game)
+    elif game.Price > root.Game.Price:
+        root.L = insert_price_bst(root.L, game)
+    else:
+        root.R = insert_price_bst(root.R, game)
+    return root
+
+def generate_price_bst(games : list):
+    bst = None
+    for game in games:
+        bst = insert_price_bst(bst, game)
+    return bst
+
+games = generate_games()
+bst = generate_price_bst(games)
+print_bst(bst)
+
+input()
