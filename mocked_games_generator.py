e160c294ef4ba16ad9ee81089f3c09c7581d7aab
Raisson Souza <raissonrai@gmail.com>
2023-11-02 22:53:39 -0300

    Gerador de jogos mockados para teste implementado

---
+++ mocked_games_generator.py
@@ -0,0 +1,11 @@
+from generate_random_games import generate_random_games
+
+def generate_games(quantity : int):
+    games = generate_random_games(quantity)
+
+    file = open("mocked_games.txt", "w")
+
+    for game in games:
+        file.write(f"{ game.Id }#{ game.Title }#{ game.Developer }#{ game.Price }#{ game.Genres }\n")
+
+generate_games(150000)
